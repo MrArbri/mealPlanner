e2fcac1965464aef2a964fb412fa5eeae42b52d9
Jaskaran Singh <jasks123@gmail.com>
2023-12-12 11:42:09 +0100

    made contact and about page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,4 +16,20 @@ class HomeController extends AbstractController
             'meals' => $mealRepository->findAll(),
         ]);
     }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(MealRepository $mealRepository): Response
+    {
+        return $this->render('home/about.html.twig', [
+            'meals' => $mealRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(MealRepository $mealRepository): Response
+    {
+        return $this->render('home/contact.html.twig', [
+            'meals' => $mealRepository->findAll(),
+        ]);
+    }
 }
