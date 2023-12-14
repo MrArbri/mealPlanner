95b8f0ffe136d0c458e54724b6c148b35e74ec51
borisvlaovic <borisvk4@gmail.com>
2023-12-14 15:43:05 +0100

    Fixed Changes

--- src/Form/PlannerType.php
+++ src/Form/PlannerType.php
@@ -12,7 +12,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Range;
 
-class MealType extends AbstractType
+class PlannerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
